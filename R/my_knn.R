06a5ff2a6fee55a1c7b3a00c908687dc9314af87
Lejin Tian <ljtian20@fudan.edu.cn>
2022-07-01 12:09:31 +0800

    20220701

--- R/my_knn.R
+++ R/my_knn.R
@@ -1,3 +1,47 @@
+weighted_knn <- function(nn.idx,nn.dist,labels){
+  sapply(1:nrow(nn.idx),function(row_idx){
+    one_nn_idx <- nn.idx[row_idx,]
+    one_nn_dist <- nn.dist[row_idx,]
+    nn_labels <- labels[one_nn_idx]
+    one_nn_weight <- (max(one_nn_dist) - one_nn_dist) / (max(one_nn_dist) - min(one_nn_dist))
+    labels_impact <- sapply(unique(nn_labels),function(label){
+      sum(one_nn_weight[which(nn_labels == label)])
+    })
+    pred <- names(labels_impact)[which.max(labels_impact)]
+    score <- max(labels_impact)/sum(labels_impact)
+    names(score) <- pred
+    return(score)
+  })
+}
+
+ss_knn <- function(nn.idx,nn.dist,labels = labels){
+  sapply(1:nrow(nn.idx),function(row_idx){
+    one_nn_idx <- nn.idx[row_idx,]
+    one_nn_dist <- nn.dist[row_idx,]
+    nn_labels <- local_ref_labels[one_nn_idx]
+    labels_impact <- sapply(unique(nn_labels),function(label){
+      exp(-one_nn_dist[which(nn_labels == label)]) %>% mean()
+    })
+    pred <- names(labels_impact)[which.max(labels_impact)]
+    score <- max(labels_impact)/sum(labels_impact)
+    names(score) <- pred
+    return(score)
+  })
+}
+
+normal_knn <- function(nn.idx,nn.dist,labels = labels){
+  sapply(1:nrow(nn.idx),function(row_idx){
+    one_nn_idx <- nn.idx[row_idx,]
+    nn_labels <- local_ref_labels[one_nn_idx]
+    TAB <- table(nn_labels)
+    pred <- names(TAB)[which.max(TAB)]
+    score <- max(TAB)/sum(TAB)
+    names(score) <- pred
+    return(score)
+  })
+}
+
+
 #' Several K Nearest Neighbors (KNN) methods
 #'
 #' @param train The train dataset whose rows are samples and columns are features
@@ -26,50 +70,10 @@ my_knn <- function(train,labels,test,k,method = 'weighted',dist.metric = 'cosine
     message(paste0('Predicting labels using ',method,' knn method'))
   }
   if (method == 'weighted'){
-    weighted_knn <- function(nn.idx,nn.dist,labels = labels){
-      sapply(1:nrow(nn.idx),function(row_idx){
-        one_nn_idx <- nn.idx[row_idx,]
-        one_nn_dist <- nn.dist[row_idx,]
-        nn_labels <- labels[one_nn_idx]
-        one_nn_weight <- (max(one_nn_dist) - one_nn_dist) / (max(one_nn_dist) - min(one_nn_dist))
-        labels_impact <- sapply(unique(nn_labels),function(label){
-          sum(one_nn_weight[which(nn_labels == label)])
-        })
-        pred <- names(labels_impact)[which.max(labels_impact)]
-        score <- max(labels_impact)/sum(labels_impact)
-        names(score) <- pred
-        return(score)
-      })
-    }
     raw_pred <- weighted_knn(nn.idx,nn.dist,labels)
   } else if (method == 'Shieh'){
-    ss_knn <- function(nn.idx,nn.dist,labels = labels){
-      sapply(1:nrow(nn.idx),function(row_idx){
-        one_nn_idx <- nn.idx[row_idx,]
-        one_nn_dist <- nn.dist[row_idx,]
-        nn_labels <- local_ref_labels[one_nn_idx]
-        labels_impact <- sapply(unique(nn_labels),function(label){
-          exp(-one_nn_dist[which(nn_labels == label)]) %>% mean()
-        })
-        pred <- names(labels_impact)[which.max(labels_impact)]
-        score <- max(labels_impact)/sum(labels_impact)
-        names(score) <- pred
-        return(score)
-      })
-    }
     raw_pred <- ss_knn(nn.idx,nn.dist,labels)
   } else if (method == 'normal'){
-    normal_knn <- function(nn.idx,nn.dist,labels = labels){
-      sapply(1:nrow(nn.idx),function(row_idx){
-        one_nn_idx <- nn.idx[row_idx,]
-        nn_labels <- local_ref_labels[one_nn_idx]
-        TAB <- table(nn_labels)
-        pred <- names(TAB)[which.max(TAB)]
-        score <- max(TAB)/sum(TAB)
-        names(score) <- pred
-        return(score)
-      })
-    }
     raw_pred <- normal_knn(nn.idx,nn.dist,labels)
   }
   out <- data.frame(pred_labs = names(raw_pred),
