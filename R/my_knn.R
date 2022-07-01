44aa696fea8f1d7547748a4b216aa01d096c09c4
Lejin Tian <ljtian20@fudan.edu.cn>
2022-07-01 12:44:42 +0800

    20220701

--- R/my_knn.R
+++ R/my_knn.R
@@ -14,7 +14,7 @@ weighted_knn <- function(nn.idx,nn.dist,labels){
   })
 }
 
-ss_knn <- function(nn.idx,nn.dist,labels = labels){
+ss_knn <- function(nn.idx,nn.dist,labels){
   sapply(1:nrow(nn.idx),function(row_idx){
     one_nn_idx <- nn.idx[row_idx,]
     one_nn_dist <- nn.dist[row_idx,]
@@ -29,7 +29,7 @@ ss_knn <- function(nn.idx,nn.dist,labels = labels){
   })
 }
 
-normal_knn <- function(nn.idx,nn.dist,labels = labels){
+normal_knn <- function(nn.idx,nn.dist,labels){
   sapply(1:nrow(nn.idx),function(row_idx){
     one_nn_idx <- nn.idx[row_idx,]
     nn_labels <- local_ref_labels[one_nn_idx]
@@ -41,6 +41,21 @@ normal_knn <- function(nn.idx,nn.dist,labels = labels){
   })
 }
 
+self_weighted_knn <- function(nn.idx,nn.dist,labels){
+  sapply(1:nrow(nn.idx),function(row_idx){
+    one_nn_idx <- nn.idx[row_idx,][-1]
+    one_nn_dist <- nn.dist[row_idx,][-1]
+    nn_labels <- labels[one_nn_idx][-1]
+    one_nn_weight <- (max(one_nn_dist) - one_nn_dist) / (max(one_nn_dist) - min(one_nn_dist))
+    labels_impact <- sapply(unique(nn_labels),function(label){
+      sum(one_nn_weight[which(nn_labels == label)])
+    })
+    pred <- names(labels_impact)[which.max(labels_impact)]
+    score <- max(labels_impact)/sum(labels_impact)
+    names(score) <- pred
+    return(score)
+  })
+}
 
 #' Several K Nearest Neighbors (KNN) methods
 #'
@@ -48,7 +63,14 @@ normal_knn <- function(nn.idx,nn.dist,labels = labels){
 #' @param labels Labels of the train dataset
 #' @param test The test dataset whose rows are samples and columns are features
 #' @param k Number of neighbors
-#' @param method Knn method; can be one of "weighted", "Shieh", "normal"
+#' @param method Knn method; can be one of "weighted", "Shieh", "normal","self_weighted"
+#'  \itemize{
+#'   \item{'weighted': }{weighted knn}
+#'   \item{'Shieh': }{https://ieeexplore.ieee.org/document/6298627}
+#'   \item{'normal': }{normal knn}
+#'   \item{'self_weighted': }{using all labeled data as reference to predict the same data as unlabeled query,
+#'    the nearest neighbor of one sample is not itself.}
+#' }
 #' @param dist.metric Distance metric; can be one of "euclidean", "cosine", "manhattan",
 # "hamming"
 #' @param verbose Whether to display messages
