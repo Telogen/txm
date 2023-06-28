0b0fece9a16f3f879fa3874a29bf3736a9e3ef39
Lejin Tian <ljtian20@fudan.edu.cn>
2023-06-28 16:57:33 +0800

    20230628

--- R/my_read_10X.R
+++ R/my_read_10X.R
@@ -8,7 +8,7 @@
 #' @export
 #'
 my_read_10X <- function (feature_file, cell_file, mtx_file) {
-  counts <- readMM(file = mtx_file)
+  counts <- Matrix::readMM(file = mtx_file)
   cell.barcodes <- as.data.frame(data.table::fread(cell_file, header = FALSE))$V1
   feature.names <- as.data.frame(data.table::fread(feature_file, header = FALSE))$V1
   dimnames(counts) <- list(feature.names,cell.barcodes)
