f1fc22a945d524fca0c23a36267138e51f409eca
Lejin Tian <ljtian20@fudan.edu.cn>
2024-03-11 12:53:54 +0800

    20240311

--- R/myGO.R
+++ R/myGO.R
@@ -7,15 +7,19 @@
 #' @export
 #'
 #' @examples
-myGO <- function(genes){
+myGO <- function(genes,title = NULL,term_num = 10){
   library(clusterProfiler)
   library(org.Hs.eg.db)
   x <- genes
-  eg <- bitr(x, fromType="SYMBOL", toType=c("ENTREZID","ENSEMBL"), OrgDb="org.Hs.eg.db"); head(eg)
+  eg <- clusterProfiler::bitr(x, fromType="SYMBOL", toType=c("ENTREZID","ENSEMBL"),
+                              OrgDb="org.Hs.eg.db")
   genelist <- eg$ENTREZID
   genelist <- genelist[-duplicated(genelist)]
-  go <- enrichGO(genelist, OrgDb = org.Hs.eg.db, ont='ALL',pAdjustMethod = 'BH',pvalueCutoff = 0.05,
-                 qvalueCutoff = 0.2,keyType = 'ENTREZID')
-  barplot(go,showCategory=20,drop=T)
-  dotplot(go,showCategory=50)
+  go <- enrichGO(genelist, OrgDb = org.Hs.eg.db, ont='ALL',pAdjustMethod = 'BH',
+                 pvalueCutoff = 0.05,qvalueCutoff = 0.2,keyType = 'ENTREZID')
+  if(is.null(go)){
+    return('no significant GO term')
+  } else{
+    dotplot(go,showCategory = term_num) + ggtitle(title)
+  }
 }
